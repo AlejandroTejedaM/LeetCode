d7839041f9b6f4cea150621e78080164da49542f
alex2227 <alex2227@hotmail.com>
2025-01-22 19:02:11 -0600

    Solved 12 cases of 121

--- src/main/java/alex/course/calculateDigitSumOfString/Solution.java
+++ src/main/java/alex/course/calculateDigitSumOfString/Solution.java
@@ -3,50 +3,72 @@ package alex.course.calculateDigitSumOfString;
 import java.util.ArrayList;
 
 public class Solution {
-    /*public static String divGroupsByString(String s) {
-        if (s.length() == 0) {
-            return "";
-        } else {
-            s = s.substring(0, s.length() - 1);
-        }
-        return divGroupsByString(s) + s;
-    }*/
 
     public static String digitSum(String s, int k) {
-        ArrayList<String> groups = new ArrayList<>();
+        String groups = "";
         int numberOfGroups = (int) Math.ceil((double) s.length() / k);
-        //Groups division
-        String sFirstRound = s;
-        for (int i = 0; i <= sFirstRound.length() - 1; i += k) {
+        String stringRound = s;
+        int i;
+        int l;
+        int j;
+        for (i = 0; i <= stringRound.length() - 1; i += k) {
             String group = "";
-            if (groups.size() == numberOfGroups - 1) {
-                group += s.substring(i, s.length() - 1 + 1);
-            } else {
-                group += s.substring(i, (i + k));
+            if(s.length() == k){
+                return s;
             }
-            if(){
-
+            var tamannio = groups.length();
+            if (tamannio >= numberOfGroups - 1) {
+                group += stringRound.substring(i, stringRound.length() - 1 + 1);
+                System.out.println(group);
+                int sum = 0;
+                for (l = 0; l < group.length(); l++) {
+                    sum += Integer.parseInt(String.valueOf(group.charAt(l)));
+                }
+                String sumString = String.valueOf(sum);
+                if (!(sumString.length() < k)) {
+                    for (j = 0; j <= sumString.length() - 1; j += k) {
+                        groups += sumString.substring(j, (j + k));
+                    }
+                } else {
+                    groups += sumString;
+                }
+            } else {
+                group += stringRound.substring(i, (i + k));
+                System.out.println(group);
+                int sum = 0;
+                for (l = 0; l < group.length(); l++) {
+                    sum += Integer.parseInt(String.valueOf(group.charAt(l)));
+                }
+                String sumString = String.valueOf(sum);
+                if (!(sumString.length() < k)) {
+                    for (j = 0; j <= sumString.length() - 1; j += k) {
+                        groups += sumString.substring(j, (j + k));
+                    }
+                } else {
+                    groups += sumString;
+                }
+            }
+            if (i + k > stringRound.length() && groups.length() > k) {
+                i = -k;
+                numberOfGroups = (int) Math.ceil((double) groups.length() / k);
+                stringRound = "";
+                stringRound = groups;
+                groups = "";
             }
         }
-        //Calculate the digit sum
-        sFirstRound = "";
-        for (String a : groups) {
-            int sum = 0;
-            for (int i = 0; i < a.length(); i++) {
-                System.out.println(a.charAt(i));
-                sum += Integer.parseInt(String.valueOf(a.charAt(i)));
-            }
-            if (sum % k == 0) {
-
-            } else {
-                
-            }
-            sFirstRound += sum;
-        }
-        return sFirstRound;
+        return groups;
     }
 
     public static void main(String[] args) {
-        System.out.println(digitSum("111112223", 3));
+        System.out.println(digitSum("01234567890", 2));
+        //01 23 45 67 89 0
+        //1 5 9 13 17 0
+        //15 91 31 70
+        //6 10 4 7
+        //61 04 7
+        //7 4 7
+        //74 7
+        //11 7
+        //27
     }
 }
